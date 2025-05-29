03f52b4f4fdc106d9a29c38d98747bfcffdfdc33
juliohtm08 <jhtomoyuki345@gmail.com>
2025-05-29 14:10:23 -0300

    add register user and login

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+// Models
+const User = require('./models/User');
+
 const app = express();
+
+// Config JSON response
+app.use(express.json());
 
 // Open Route - Public Route
 app.get('/', (req, res) => {
@@ -14,6 +20,82 @@ app.get('/', (req, res) => {
 // Credencials
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+
+// Register User
+app.post('/auth/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  // validations
+  if (!name) {
+    return res.status(422).json({ msg: 'O nome é obrigatório' });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: 'O email é obrigatório' });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: 'A senha é obrigatório' });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(422).json({ msg: 'As senhas não conferem' });
+  }
+
+  // check if user exists
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.status(422).json({ msg: 'Email já existente' });
+  }
+
+  // create password
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+
+    res.status(201).json({ msg: 'Usuário criado com sucesso!' });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
+// Login User
+app.post('/auth/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  //validations
+  if (!email) {
+    return res.status(422).json({ msg: 'O email é obrigatório' });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: 'A senha é obrigatório' });
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: 'Usuário não encontrado' });
+  }
+
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: 'Senha inválida' });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
