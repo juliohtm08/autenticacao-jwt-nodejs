f876c45a6d970d3fb36902a54a75c8acb79a8666
juliohtm08 <jhtomoyuki345@gmail.com>
2025-05-29 13:31:50 -0300

    connect to database

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config;
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
@@ -6,12 +6,26 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+// Open Route - Public Route
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'API funcionando!' });
 });
 
+// Credencials
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
-);
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.0lg9ye8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(
+        `Banco de dados conectado e servidor rodando em http://localhost:${PORT}`
+      )
+    );
+  })
+  .catch((err) => console.log(err));
