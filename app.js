ec4020265dedb37069fccda52933910632135a96
juliohtm08 <jhtomoyuki345@gmail.com>
2025-05-29 14:42:11 -0300

    Add JWT authentication

--- app.js
+++ app.js
@@ -21,6 +21,39 @@ app.get('/', (req, res) => {
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+// Private Route
+app.get('/user/:id', checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  //chck if user exists
+  const user = await User.findById(id, '-password');
+
+  if (!user) {
+    return res.status(404).json({ msg: 'usuário não encontrado' });
+  }
+
+  res.status(200).json({ user });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: 'acesso negado' });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: 'token inválido' });
+  }
+}
+
 // Register User
 app.post('/auth/register', async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
@@ -94,6 +127,20 @@ app.post('/auth/login', async (req, res) => {
 
   if (!checkPassword) {
     return res.status(422).json({ msg: 'Senha inválida' });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: 'autenticação realizada com sucesso', token });
+  } catch (error) {
+    res.status(500).json({ msg: error });
   }
 });
 
